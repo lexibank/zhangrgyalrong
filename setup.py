34149fd55f81a088974bc8e9f6a9916f9c92ae45
MacyL <wumeishin@gmail.com>
2019-11-07 14:18:59 +0100

    construct lexibank skeleton

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import json
+
+
+with open('metadata.json') as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_zhanggyalrong',
+    version="1.0",
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_zhanggyalrong'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'zhanggyalrong=lexibank_zhanggyalrong:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank>=1.1.1',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
