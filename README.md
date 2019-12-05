0b310736beda0e313b6daf751e17e0c3e71c1135
lingulist <mattis.list@lingpy.org>
2019-12-05 16:31:24 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# Zhangetal2019
-Old Chinese Gyalrong cognates
+# A study of cognates between Gyalrong and Old Chinese
+
+Cite the source dataset as
+
+> Zhang et al 2019
+
+This dataset is licensed under a https://creativecommons.org/licenses/by-nc/4.0/ license
+
+Available online at https://pure.mpg.de/pubman/faces/ViewItemOverviewPage.jsp?itemId=item_3149499
+
+## Statistics
+
+
+![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
+![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+![BIPA: 87%](https://img.shields.io/badge/BIPA-87%25-yellowgreen.svg "BIPA: 87%")
+![CLTS SoundClass: 87%](https://img.shields.io/badge/CLTS%20SoundClass-87%25-yellowgreen.svg "CLTS SoundClass: 87%")
+
+- **Varieties:** 7
+- **Concepts:** 120
+- **Lexemes:** 603
+- **Sources:** 1
+- **Synonymy:** 1.01
+- **Cognacy:** 603 cognates in 122 cognate sets (0 singletons)
+- **Cognate Diversity:** 0.00
+- **Invalid lexemes:** 0
+- **Tokens:** 2,710
+- **Segments:** 127 (17 BIPA errors, 17 CTLS sound class errors, 110 CLTS modified)
+- **Inventory size (avg):** 40.29
--- lexibank_zhanggyalrong.py
+++ lexibank_zhanggyalrong.py
@@ -2,70 +2,72 @@ from collections import OrderedDict
 from pathlib import Path
 
 import attr
+from clldutils.misc import slug
 from pylexibank import Concept, Language
 from pylexibank.dataset import Dataset as MyDataset
 from pylexibank.forms import FormSpec
-from pylexibank.util import progressbar
+from pylexibank import progressbar
 
 @attr.s
 class CustomConcept(Concept):
     Chinese_Gloss = attr.ib(default=None)
+    Gloss_in_Source = attr.ib(default=None)
 
 @attr.s
 class CustomLanguage(Language):
-    Latitude = attr.ib(default=None)
-    Longitude = attr.ib(default=None)
-    ChineseName = attr.ib(default=None)
-    SubGroup = attr.ib(default=None)
+    Chinese_Name = attr.ib(default=None)
     Family = attr.ib(default="Sino-Tibetan")
-    DataSource = attr.ib(default=None)
-    Autonym = attr.ib(default=None)
-    ISO = attr.ib(default=None)
     Name_in_Source = attr.ib(default=None)
-    Location = attr.ib(default=None)
 
 class Dataset(MyDataset):
     dir = Path(__file__).parent
     id = "zhanggyalrong"
     concept_class = CustomConcept
     language_class = CustomLanguage
-    # NEED TO FURTHER EDIT
-    #
-    # form_spec = FormSpec(
-    #     missing_data=["*", "---", "-"],
-    #     separators=";/,",
-    #     strip_inside_brackets=True,
-    #     brackets={"(": ")"},
-    # )
+    form_spec = FormSpec(
+        missing_data=[''],
+        separators=";/,",
+        strip_inside_brackets=False,
+        brackets={'(': ')', '[': ']'},
+        replacements=[
+            ('-', '+'), 
+            ('*', ''),
+            ('[', ''),
+            (']', ''),
+            ('(', ''),
+            (')', '')
+            ]
+    )
 
     def cmd_makecldf(self, args):
-        data = self.raw_dir.read_csv("zhang2019-oc-rgyal.tsv", dicts=True)
-        languages, concepts = {}, {}
-        # NEED TO EDIT
-        # for concept in self.conceptlists[0].concepts.values():
-        #     args.writer.add_concept(
-        #         ID=concept.number,
-        #         Name=concept.gloss,
-        #         Concepticon_ID=concept.concepticon_id,
-        #         Concepticon_Gloss=concept.concepticon_gloss,
-        #         Chinese_Gloss=concept.attributes["chinese"],
-        #     )
-        #     concepts[concept.attributes["chinese"]] = concept.number
-        #
-        # args.writer.add_languages()
-        #
-        # languages = OrderedDict([(k["Name"], k["ID"]) for k in self.languages])
-        # args.writer.add_sources(*self.raw_dir.read_bib())
-        # missing = {}
-        # for cgloss, entry in progressbar(enumerate(data), desc="cldfify the data", total=len(data)):
-        #     if entry["Chinese gloss"] in concepts.keys():
-        #         for language in languages:
-        #             if entry[language].strip():
-        #                 args.writer.add_lexemes(
-        #                     Language_ID=languages[language],
-        #                     Parameter_ID=concepts[entry["Chinese gloss"]],
-        #                     Value=entry[language],
-        #                     Source=["Chen2013"],
-        #                 )
-        #     else:
-        #         missing[entry["Chinese gloss"]] += 1
+        args.writer.add_sources()
+        data = self.raw_dir.read_csv("zhang2019-oc-rgyal.tsv", dicts=True,
+                delimiter="\t")
+        languages = args.writer.add_languages(
+                lookup_factory='Name')
+        concepts = {}
+        for concept in self.concepts:
+            idx = concept['ID'].split('-')[-1]+'_'+slug(concept['ENGLISH'])
+            args.writer.add_concept(
+                    ID=idx,
+                    Name=concept['ENGLISH'],
+                    Chinese_Gloss=concept['CHINESE'],
+                    Gloss_in_Source=concept['GLOSS_IN_SOURCE']
+                    )
+            concepts[concept['CHINESE']] = idx
+
+        for cogid_, entry in progressbar(
+                enumerate(data), desc="cldfify the data", total=len(data)
+                ):
+            cogid = cogid_ + 1
+            for language in languages:
+                if entry[language].strip():
+                    for row in args.writer.add_forms_from_value(
+                        Language_ID=languages[language],
+                        Parameter_ID=concepts[entry["Chinese_character"]],
+                        Value=entry[language],
+                        Source=["Zhang2019"],
+                        ):
+                        args.writer.add_cognate(
+                                lexeme=row,
+                                Cognateset_ID=cogid)
